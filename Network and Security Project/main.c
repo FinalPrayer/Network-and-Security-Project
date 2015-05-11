508d87e275a4d8ff7e8778c551af8e103da9857b
FinalPrayer <zhongjiqidao@Shuras-MacBook-Pro.local>
2015-05-11 23:59:00 +0800

    Initial Commit

---
+++ Network and Security Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Network and Security Project
+//
+//  Created by Ricardo Shura on 5/11/15.
+//  Copyright (c) 2015 Ricardo Shura. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
