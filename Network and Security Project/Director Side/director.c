080d0cc16bf18373121cb6934c7915c8266cc5cc
FinalPrayer <zhongjiqidao@Shuras-MacBook-Pro.local>
2015-05-21 17:33:46 +0800

    director moved to main file.
    This application has been divided to the main file.

---
+++ Network and Security Project/Director Side/director.c
@@ -0,0 +1,14 @@
+/*
+ Network and security project
+ Director side.
+ 
+ main network module.
+ */
+
+#include "director.h"
+
+int main() {
+    //straightly start the network module.
+    network_module();
+    return 0;
+}
